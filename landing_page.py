d16e6c41192d70ea88c8ef49e241c7f513977510
a.gamal <a.gamal@cequens.com>
2019-04-01 13:46:44 +0200

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 @app.route("/landingPage", methods=['GET'])
 def landingPage():
     return render_template("landingPage.html")
+# app.run(host='0.0.0.0', port=8002)
 app.run(host='0.0.0.0', port=8002)
