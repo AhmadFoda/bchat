3a8041819fdcb5bfd0213b105ce773f08b6f976e
a.gamal <a.gamal@cequens.com>
2019-04-02 11:59:44 +0200

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
-
-
-@app.route('/landingPage')
-def landingPage():
-    return render_template("landingPage.html")
-
-if __name__ == '__main__':
-    app.run()
+# from flask import Flask, request, render_template
+# app = Flask(__name__)
+#
+#
+# # @app.route('/landingPage')
+# # def landingPage():
+# #     return render_template("landingPage.html")
+# #
+# # if __name__ == '__main__':
+# #     app.run()
