9c6889a8bd72ea3c8deb63a1349a846725851fc9
a.gamal <a.gamal@cequens.com>
2019-04-02 11:51:00 +0200

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def landingPage():
-    return render_template("landingPage.html")
+    return ("helloo")
 
 if __name__ == '__main__':
     app.run()
--- landing_page.py
+++ landing_page.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
+
+
 @app.route('/landingPage')
 def landingPage():
     return render_template("landingPage.html")
-# app.run(host='0.0.0.0', port=8002)
+
 if __name__ == '__main__':
     app.run()
