8eabed9c6d6702c9f2cea24d2318b788f39c53db
a.gamal <a.gamal@cequens.com>
2019-04-02 11:54:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def landingPage():
     return ("helloo")
+@app.route('/landingPage')
+def landingPage():
+    return render_template("landingPage.html")
 
 if __name__ == '__main__':
     app.run()
