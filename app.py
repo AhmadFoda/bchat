457d51f782fb8cb226f3a439b4e5549ed82c9372
a.gamal <a.gamal@cequens.com>
2019-06-27 17:33:03 +0200

    ss

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/message')
 def landingPage():
-    
-    return receive_message();
+    return receive_large_interactive_payload();
 
 if __name__ == '__main__':
     app.run()
