68c019321b55ba589543d9270be4dd8709eb1f62
a.gamal <ahmadfoda92@gmail.com>
2019-09-04 16:00:38 +0300

    a

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018 Apple, Inc.
 # All Rights Reserved.
 
-BUSINESS_CHAT_SERVER = "https://mspgw-int.push.apple.com/v1"
+BUSINESS_CHAT_SERVER = "https://mspgw.push.apple.com/v1"
 
 CSP_ID = "c242a3c7-a0e3-4688-9a03-fc3ecd18f409"
-BIZ_ID = "e937376b-2bd4-4e33-bade-edaa9fab0ef4"
+BIZ_ID = "aaf411eb-6ee9-44ac-8f21-6151f976e9d1"
 
 SECRET = "yjq7zIrBBlIas7BBIJfbep8aLnvc6r8gEjvOxXHesew="
 
